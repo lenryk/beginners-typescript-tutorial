ba15f5f9c567bcd7db86ab58b2ae1ab978cf3305
Sam <lenry101@gmail.com>
2023-08-21 11:44:19 +0100

    exercise 13

--- src/13-catch-blocks.problem.ts
+++ src/13-catch-blocks.problem.ts
@@ -6,7 +6,12 @@ const tryCatchDemo = (state: "fail" | "succeed") => {
       throw new Error("Failure!");
     }
   } catch (e) {
-    return e.message;
+    // all errors from catch are unknown
+    // we need to add a type guard to check it is an error object so we can access the message prop
+    if(e instanceof Error) {
+      return e.message;
+    }
+    // add fallback here in case it is not an error object
   }
 };
 
