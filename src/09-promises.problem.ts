d3ab442c430b18c7fb91a1afc30fb6309549c4ca
Sam <lenry101@gmail.com>
2023-08-21 10:17:59 +0100

    exercise 9

--- src/09-promises.problem.ts
+++ src/09-promises.problem.ts
@@ -8,11 +8,13 @@ interface LukeSkywalker {
   birth_year: string;
   gender: string;
 }
-
-export const fetchLukeSkywalker = async (): LukeSkywalker => {
+// async functions always return promises
+// to set the return type of a promise use Promise<type>
+export const fetchLukeSkywalker = async (): Promise<LukeSkywalker> => {
   const data = await fetch("https://swapi.dev/api/people/1").then((res) => {
     return res.json();
   });
 
   return data;
+  // return data as LukeSkywalker
 };
