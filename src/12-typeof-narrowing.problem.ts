d0dd6ce51e689cea1acfe675531318f021577ce3
Sam <lenry101@gmail.com>
2023-08-21 11:40:39 +0100

    exercise 12

--- src/12-typeof-narrowing.problem.ts
+++ src/12-typeof-narrowing.problem.ts
@@ -1,6 +1,14 @@
 import { expect, it } from "vitest";
 
-const coerceAmount = (amount: number | { amount: number }) => {};
+const coerceAmount = (amount: number | { amount: number }) => {
+  // add a type guard to check if the param is of type number
+  if(typeof amount === "number") {
+    return amount
+  } else {
+    // if it is not of type number then we know it's an object
+    return amount.amount
+  }
+};
 
 it("Should return the amount when passed an object", () => {
   expect(coerceAmount({ amount: 20 })).toEqual(20);
